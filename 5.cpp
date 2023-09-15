3700d63e846dda57e5bb1c06c06e825a6569b352
ChavronV <97131681+WhatWouldDarknessSay@users.noreply.github.com>
2023-09-15 22:29:33 +0300

    fix 6.cpp

--- 5.cpp
+++ 5.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 using namespace std;
 int main(){
-    int x[3] = {-3, 0, 3};
+    double x[3] = {-3, 0, 3};
     double long y[3] = {0, 0, 0};
     int a = 4;
     for (int i=0; i < 3; ++i){
--- 6.cpp
+++ 6.cpp
@@ -2,24 +2,24 @@
 #include<cmath>
 using namespace std;
 int main(){
-    long a, b, c, r, d, ans1x, ans1y, ans2x, ans2y;
-    cout <<"Расчет точек пересечения Ax + By = C и x²+y²=R²" << endl << "Введите коэффициент A";
+    double a, b, c, r, d, ans1x, ans1y, ans2x, ans2y;
+    cout <<"Расчет точек пересечения Ax + By = C и x²+y²=R²" << endl << "Введите коэффициент A:";
     cin >> a;
-    cout << "Введите коэффициент B";
+    cout << "Введите коэффициент B:";
     cin >> b;
-    cout << "Введите коэффициент C";
+    cout << "Введите коэффициент C:";
     cin >> c;
-    cout << "Введите коэффициент R";
+    cout << "Введите коэффициент R:";
     cin >> r;
-    d = 4 * pow(b, 2) * pow(c, 2) + 4 * (pow(b, 2) + pow(a, 2)) * pow(r, 2) * pow(a, 2);
+    d = 4 * pow(b, 2) * pow(c, 2) - 4 * (pow(b, 2) + pow(a, 2)) * (-1.0 * pow(r, 2) * pow(a, 2) + pow(c, 2));
     if (d > 0) {
-        ans1y = (2 * b * c + sqrt(d)) / (2 * (pow(b, 2) + pow(a, 2)));
-        ans2y = (2 * b * c - sqrt(d)) / (2 * (pow(b, 2) + pow(a, 2)));
+        ans1y = (2.0 * b * c + pow(d, 0.5)) / (2.0  * (pow(b, 2) + pow(a, 2)));
+        ans2y = (2 * b * c - pow(d, 0.5)) / (2 * (pow(b, 2) + pow(a, 2)));
         ans1x = (c - b * ans1y) / a;
         ans2x = (c - b * ans2y) / a;
         cout << "(" << ans1x << "," << ans1y << ")" << endl << "(" << ans2x << "," << ans2y << ")" << endl;
     } else if(d == 0) {
-        ans1y = (2 * b * c + sqrt(d)) / (2 * (pow(b, 2) + pow(a, 2)));
+        ans1y = (2 * b * c + pow(d, 0.5)) / (2 * (pow(b, 2) + pow(a, 2)));
         ans1x = (c - b * ans1y) / a;
         cout << "(" << ans1x << "," << ans1y << ")" << endl;
     } else {
